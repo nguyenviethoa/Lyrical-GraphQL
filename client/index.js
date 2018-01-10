36ad299e45652115820af63828fa46db589cf0ef
nguyenviethoa <nguyenviethoa1984@gmail.com>
2018-01-10 13:40:06 +0700

    correct init apollo-client

--- client/index.js
+++ client/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-// const client = new ApolloClient({});
+const client = new ApolloClient({
+	// By default, this client will send queries to the
+  //  `/graphql` endpoint on the same host
+  link: new HttpLink(),
+  cache: new InMemoryCache()
+});
 
 const Root = () => {
   return <div>Lyrical</div>
